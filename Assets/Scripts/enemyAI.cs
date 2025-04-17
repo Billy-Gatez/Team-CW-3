23ba243dacb6ec55e5a7d1f549506074f12f1daa
George Carter <gtcarter@student.fullsail.edu>
2025-04-17 09:06:59 -0500

    All
    All

---
+++ Assets/Scripts/enemyAI.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class enemyAI : MonoBehaviour, IDamage
+{
+
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;   
+
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+
+    bool playerInRange;
+
+    float shootTimer;
+
+    Color colorOrig;
+
+    Vector3 playerDir;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorOrig = model.material.color;
+        gamemanager.instance.updateGameGoal(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange)
+        {
+            playerDir = (gamemanager.instance.player.transform.position - transform.position);
+
+            agent.SetDestination(gamemanager.instance.player.transform.position);
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                faceTarget();
+            }
+
+            shootTimer += Time.deltaTime;
+
+            if (shootTimer >= shootRate)
+            {
+                shoot();
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        StartCoroutine(flashRed());
+
+        agent.SetDestination(gamemanager.instance.player.transform.position);
+
+        if (HP < 0)
+        {
+            gamemanager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootPos.position, transform.rotation);
+
+    }
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+}
